e777c90645d922e0f57c2df907296613a781ec68
mrchigozieike <mrchigozieike@gmail.com>
2023-01-16 16:09:43 +0100

    W02 Prove: Developer—C# Programming Parts 3–5

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        var first_name = Console.ReadLine();
+        
     }
-}
+} 
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
-using System;
-
-class Program
+namespace HelloWorld
 {
-    static void Main(string[] args)
+    public class Resume
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+    public string CompanyName{ get; set; }
+    public String JobTitle{get; set;} 
+    public int StartYear{get; set;}
+    public int EndYear{get; set;}
+
+    public Resume(string companyname, String jobtitle, int startyear, int endyear) 
+    {
+      CompanyName = companyname;
+      JobTitle = jobtitle;
+      StartYear = startyear;
+      EndYear = endyear;
     }
+}
+
+
+  class Program
+  {
+    static void Main()
+    {
+      Resume newcompany1 = new Resume("Touchcore LTD", "Developer", 2000, 2030 );
+      Console.WriteLine("CompanyName = {0} JobTitle = {1} StartYear ={2} EndYear{3}", newcompany1.CompanyName, newcompany1.JobTitle, newcompany1.StartYear, newcompany1.EndYear);
+        
+      Resume createresume = newcompany1;
+    }
+  }
 }
